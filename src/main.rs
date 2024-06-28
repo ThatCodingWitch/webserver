f7dabbbd5dcbe22a0b534046d0870ee247eb1f5c
Jara Doornbos <81758642+ThatCodingWitch@users.noreply.github.com>
2024-06-28 14:24:11 +0200

    basic functionality done
    file support for html, css, js, mp4.

---
+++ src/main.rs
@@ -0,0 +1,74 @@
+
+use std::{
+    fs,
+    io::{prelude::*, BufReader},
+    net::{TcpListener, TcpStream},
+};
+
+fn main() {
+    let listener = TcpListener::bind("127.0.0.1:8080").unwrap();
+
+    for stream in listener.incoming() {
+        let stream = stream.unwrap();
+         handle_connection(stream);
+    }
+}
+
+fn handle_connection(mut stream: TcpStream) {
+    println!("\nrequest");
+    let buf_reader = BufReader::new(&mut stream);
+    let request_line = match buf_reader.lines().next() {
+        Some(Ok(line)) => line,
+        Some(Err(e)) => {
+            eprintln!("Failed to read line from stream: {}", e);
+            return;
+        },
+        None => {
+            eprintln!("Stream is empty");
+            eprintln!("{:?}", stream);
+            return;
+        }
+    };
+
+
+    let mut filepath = request_line.split(' ').nth(1).unwrap().to_string();
+    if filepath == "/"   {
+        filepath = "/index.html".parse().unwrap()
+    }
+    let filetype = filepath.split('.').last().unwrap().to_string();
+    let mime_type = match filetype.as_str() {
+        "html" => "text/html",
+        "css" => "text/css",
+        "js" => "application/javascript",
+        "mp4" => "application/mp4",
+        _ => "text/plain",
+    };
+
+    filepath = format!("./front-end{}", filepath);
+
+    let (contents, status_line) = match fs::read(&filepath) {
+        Ok(contents) => (contents, "HTTP/1.1 200".to_string()),
+        Err(_) => {
+            filepath = "./errors/404.html".to_string();
+            match fs::read(&filepath) {
+                Ok(contents) => (contents, "HTTP/1.1 404".to_string()),
+                Err(_) => (Vec::new(), "HTTP/1.1 500".to_string())
+            }
+        }
+    };
+
+
+
+
+
+    let length = contents.len();
+    let response = format!(
+        "{}\r\nContent-Type: {}\r\nContent-Length: {}\r\n\r\n",
+        status_line, mime_type, length
+    );
+    stream.write_all(response.as_bytes()).unwrap();
+    stream.write_all(&contents).unwrap();
+}
+
+
+
